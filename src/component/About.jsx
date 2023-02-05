a88894bbaf16be1a5443b2b47eca8b926fd9a252
LuqmanAristio <aristio170802@gmail.com>
2023-02-05 22:31:23 +0700

    update about

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -14,7 +14,7 @@ export const About = () =>{
                     <h1 className="fontBold">BEST JOGLO HOUSE SPECIALIST IN BALI</h1>
 
                     <p>Joglo is a traditional Javanese house that has existed since ancient times and its preferred by many people today. 
-                        Joglo gives the feel of the past which is full of simplicity and serenity. <span>Ke-Art</span> is an experienced company engaged 
+                        Joglo gives us feel of the past which is full of simplicity and serenity. <span>Ke-Art</span> is an experienced company engaged 
                         in the sale of joglo houses with services throughout the island of Bali</p>
 
                     <h3><Link>SEE OUR GALLERY!</Link></h3>
